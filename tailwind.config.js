bf75418b00b47868e3fea6ab6cb281ac55a330d3
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2024-10-03 17:22:00 +0900

    set up

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        main: '#FFD681',
+        accent: '#9C661F',
+        base: '#FFF5E6',
+        text: '#2C1803',
+        placeholder: '#C5B6A8',
+      },
+      fontFamily: {
+        'kosugi': ['"Kosugi Maru"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        purinmania: {
+          "primary": "#9C661F",
+          "secondary": "#FFD681",
+          "accent": "#9C661F",
+          "neutral": "#2C1803",
+          "base-100": "#FFF5E6",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+    base: true,
+    styled: true,
+    utils: true,
+    darkTheme: false,
+  },
+}
