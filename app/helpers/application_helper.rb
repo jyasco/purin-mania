f4d83af05208bc0d0c6412c82ecf637eeb6b4b78
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2024-10-07 23:38:22 +0900

    flashmessage_setting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :notice
+      "bg-[#FFD681] text-[#2C1803]"
+    when :alert
+      "bg-[#9C661F] text-[#FFF5E6]"
+    when :error
+      "bg-[#C5B6A8] text-[#2C1803]"
+    else
+      "bg-[#FFF5E6] text-[#2C1803]"
+    end
+  end
 end
