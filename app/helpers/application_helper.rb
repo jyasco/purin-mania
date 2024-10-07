d463b0f4bc1a9132f05628acd31cd097d025a926
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2024-10-08 00:47:36 +0900

    lintチェック

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   def flash_background_color(type)
     case type.to_sym
     when :notice
-      "bg-[#4CAF50] text-white"
+      'bg-[#4CAF50] text-white'
     when :alert, :error
-      "bg-[#FF6B6B] text-white"
+      'bg-[#FF6B6B] text-white'
     else
-      "bg-[#FFF5E6] text-[#2C1803]"
+      'bg-[#FFF5E6] text-[#2C1803]'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
-    passwords: 'users/passwords',
+    passwords: 'users/passwords'
   }, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
