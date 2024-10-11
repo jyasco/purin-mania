66a685521d373fea3d00f8afc8321a2378d7e254
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2024-10-12 02:25:16 +0900

    ビューの作成と仮記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,22 +5,27 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { maximum: 555 }
   validates :sweetness, presence: true
   validates :firmness, presence: true
-  validates :overall_rating, presence: true, inclusion: { in: 1..5 }
+  validates :overall_rating, presence: true
 
-  enum sweetness: { sweet: 0, normal: 1, mild: 2 }
-  enum firmness: { smooth: 0, normal: 1, firm: 2 }
+  enum sweetness: { sweet: 0, medium_sweet: 1, mild: 2 }
+  enum firmness: { smooth: 0, medium_firm: 1, firm: 2 }
   enum overall_rating: {
   very_poor: 1,
   poor: 2,
-  average: 3,
+  ordinary: 3,
   good: 4,
   excellent: 5
   }
+
+  accepts_nested_attributes_for :shop
 
   # 将来的に実装予定の機能のためのコメントアウト
   # belongs_to :category
   # has_many :bookmarks
   # has_many :bookmarked_users, through: :bookmarks, source: :user
+  def rating_as_integer
+    Post.overall_ratings[overall_rating]
+  end
 
   def self.ransackable_attributes(auth_object = nil)
     ["body", "sweetness", "firmness", "overall_rating", "created_at"]
