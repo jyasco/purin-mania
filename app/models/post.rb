928810af49099a8120d543cb5c4d57d1109389a6
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2024-10-18 16:31:17 +0900

    PostImagesモデルとDB作成_マイグレート実行とアソシエーションの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,16 @@ class Post < ApplicationRecord
   
   belongs_to :user
   belongs_to :shop, optional: true
+  has_many :post_images, dependent: :destroy
+
+  accepts_nested_attributes_for :shop
+  accepts_nested_attributes_for :post_images, allow_destroy: true, reject_if: :all_blank
   
   validates :body, presence: true, length: { maximum: 555 }
   validates :sweetness, presence: true
   validates :firmness, presence: true
   validates :overall_rating, presence: true
+  validate :image_count_validation
 
   enum sweetness: { sweet: 0, medium_sweet: 1, mild: 2 }
   enum firmness: { smooth: 0, medium_firm: 1, firm: 2 }
@@ -18,8 +23,6 @@ class Post < ApplicationRecord
   good: 4,
   excellent: 5
   }
-
-  accepts_nested_attributes_for :shop
 
   # 将来的に実装予定の機能のためのコメントアウト
   # belongs_to :category
@@ -33,4 +36,11 @@ class Post < ApplicationRecord
   def self.ransackable_associations(auth_object = nil)
     ["user", "shop"]
   end
+
+  private
+
+  def image_count_validation
+    errors.add(:post_images, "は3枚まで選択可能です") if post_images.size > 3
+  end
+  
 end
