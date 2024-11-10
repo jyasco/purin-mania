6c5cad96ef897e9c27dac2afc6f0cd9fbb7f3d60
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2024-11-10 22:55:48 +0900

    add:comment_validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 50 }
+  validates :comment, length: { maximum: 50 }
 
   has_many :posts
   has_many :bookmarks, dependent: :destroy
