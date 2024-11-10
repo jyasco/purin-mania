bd763018deebfb3abbc502343ddfe05b635519f9
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2024-11-10 22:46:06 +0900

    add:マイページに好みとコメントを表示

--- app/controllers/mypage/posts_controller.rb
+++ app/controllers/mypage/posts_controller.rb
@@ -2,6 +2,7 @@ class Mypage::PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @user = current_user
     @posts = current_user.posts.includes(:user).order(created_at: :desc)
   end
 end
