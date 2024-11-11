fec23df7dc754ec14c9ea8ba4a3272c4fa8b2821
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2024-11-11 18:49:27 +0900

    パラメータの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :comment, :preferred_sweetness, :preferred_firmness])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :nickname, :avatar, :comment, :preferred_sweetness, :preferred_firmness])
   end
 
 end
