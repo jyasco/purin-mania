21b40e640e1a0fb5f317b0a565bed959d420ee48
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2024-11-11 23:29:21 +0900

    add:コントローラーの作成と記述

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
+  def show
+    @user = User.find_by(username: params[:username])
+    
+    if @user.nil?
+      redirect_to root_path, alert: "ユーザーが見つかりませんでした。"
+    elsif @user == current_user
+      redirect_to mypage_mypage_path
+    else
+      @posts = @user.posts.includes(:user).order(created_at: :desc)
+    end
+  end
