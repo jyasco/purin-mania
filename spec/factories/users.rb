62769592b489ba13b12cb9fb4b4f7c372632006f
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-08 05:03:35 +0900

    add:userのfactory bot作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:username) { |n| "user_#{n}san" }
+    sequence(:email) { |n| "user_#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
   end
 end
